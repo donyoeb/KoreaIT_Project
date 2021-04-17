05649437af8b94e5e36762190ff26156b7c8e027
donyoeb <my25092002@nate.com>
2021-04-17 18:56:46 +0900

    1 차 프로젝트 (비트 코인 웹 사이트 제작 / 로그인시 유저이름 + 로그아웃 메뉴바 내용바꾸기 수정) 3.1 ver.

--- 1차/web/server.js
+++ 1차/web/server.js
@@ -20,13 +20,34 @@ app.use(express.urlencoded({
     extended:true
 }));//post 요청의 파라미터 받기 위함.
 
+app.use(expressSession({
+    secret:"key secret",
+    resave:true,
+    saveUninitialized:true
+}));
+
 //템플릿 뷰 엔진 등록(서버 스크립트 위치 등록)
 app.set("view engine", "ejs");//등록 후에는 자동으로 무조건 view하위에서 ejs를 찾아간다.(따라서 view라는 정해진 디렉토리 무조건 존재 시켜야 한다.)
 
 //메인화면
 app.get("/coin/main", function(request, response){
     response.render("main");
+   //checkUserSession(request,response,"main"); //로그인 처리
+    //나중엔 메인만 그냥  response.render("main");
+    //처리하고 나머지에다가 로그인 처리세션 넣기
 });
+
+app.get("/coin/usermain", function(request, response){
+    
+    checkUserSession(request,response,"main"); //로그인 처리
+});
+
+app.get("/coin/main/investment", function(request, response){
+    
+    checkUserSession(request,response,"investment"); //로그인 처리
+    
+});
+
 /*
 //테트리스
 app.get("/coin/game/tetris", function(request, response){
@@ -84,6 +105,7 @@ app.post("/coin/main/login", function(request, response){
     var user_id=request.body.user_id;
     var user_pass=request.body.user_pass;
     console.log(user_id , user_pass);
+    
 
     var sql="select * from member";
     sql +=" where user_id=? and user_pass=?";
@@ -99,23 +121,49 @@ app.post("/coin/main/login", function(request, response){
                 response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
                 response.end(mymodule.getMsgBack("로그인 정보가 올바르지 않습니다"));
             }else{
-                console.log("성공");
-            
+                console.log("로그인 성공",result);
+               
+                request.session.user={
+                    member_id: result[0].member_id,
+                    user_id:result[0].user_id,
+                    user_pass:result[0].user_pass,
+                    user_name:result[0].user_name,
+                    user_money : result[0].user_money
+                };
+               
                 response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
-                response.end(mymodule.getMsgUrl("로그인성공","/coin/main"));
+                response.end(mymodule.getMsgUrl("로그인성공","/coin/usermain"));
             }
         }
         con.end();
     });
-
+  //  http://localhost:7777/coin/main
 
 
 });
 
+//로그아웃처리
+app.post("/coin/logout", function(request, response){
+    
+    delete request.session.user;
+    response.render("/coin/main");
+   
+});
 
 
-
-
+/*---------------------------------------------------
+세션 체크
+---------------------------------------------------*/
+function checkUserSession(request, response, url){
+    if(request.session.user){ //  undefined가 아니라면..
+        response.render(url, {
+            joinUser:request.session.user
+        });
+    }else{
+        response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+        response.end(mymodule.getMsgBack("로그인이 필요한 서비스입니다.","/coin/main/loginform"));
+    }
+}
 
 var server = http.createServer(app);
 server.listen(7777, function(){
